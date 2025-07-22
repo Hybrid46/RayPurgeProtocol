8321823da6d32f70d5af6587768dca58f1ef08c5
Hybrid46 <hybrid_46_2@hotmail.com>
2025-07-22 21:44:10 +0200

    Player Controller

---
+++ ComponentSystem/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using static Settings;
+
+public class PlayerController : Component, IUpdatable
+{
+    public Vector2 Direction { get; set; } = new(1, 0);
+    public Vector2 CameraPlane => new(-Direction.Y * FOV, Direction.X * FOV);
+    public float MoveSpeed { get; init; }
+    public float RotationSpeed { get; init; }
+    public float MouseRotationSpeed { get; init; }
+
+    public void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement() {  }
+    private void HandleRotation() {  }
+}
